7e8f9e34557d77a39588e14f7472a771d11ea45c
Unknown <rwoldhui@avans.nl>
2019-02-21 17:30:57 +0100

    [Init] project

---
+++ IR-Controller/main.c
@@ -0,0 +1,18 @@
+/*
+ * IR-Controller.c
+ *
+ * Created: 2/21/2019 5:30:05 PM
+ * Author : diabl
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
