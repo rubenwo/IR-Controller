eeda07c8bb5cdaf3af9a50916697760b6aaeedda
Unknown <rwoldhui@avans.nl>
2019-02-22 13:57:24 +0100

    [Added] lcd base class

---
+++ IR-Controller/lcd.c
@@ -0,0 +1,7 @@
+/*
+* lcd.c
+*
+* Created: 2/22/2019 1:56:57 PM
+*  Author: Ruben
+*/
+#include "lcd.h"
---
+++ IR-Controller/lcd.h
@@ -0,0 +1,16 @@
+/*
+* lcd.h
+*
+* Created: 2/22/2019 1:56:49 PM
+*  Author: Ruben
+*/
+
+
+#ifndef LCD_H_
+#define LCD_H_
+
+
+
+
+
+#endif /* LCD_H_ */
