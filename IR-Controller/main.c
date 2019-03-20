68863f2aa2618dce42b6773c95d876d547044a93
Ruben <rwoldhui@avans.nl>
2019-03-20 11:11:45 +0100

    [Added] new cpu architecture (atmega 328p)

---
+++ IR-Controller/main.c
@@ -0,0 +1,18 @@
+/*
+* IR-Controller.c
+*
+* Created: 3/20/2019 11:10:12 AM
+* Author : Ruben
+*/
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+	/* Replace with your application code */
+	while (1)
+	{
+	}
+}
+
