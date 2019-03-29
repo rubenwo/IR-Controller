14ff844ceebe999de782d1e2ab26695be2caa1a0
Unknown <bart.vanes1@gmail.com>
2019-03-29 01:11:15 +0100

    Al weer helemaal klaar met dit project

--- IR-Controller/main.c
+++ IR-Controller/main.c
@@ -4,15 +4,27 @@
 * Created: 3/20/2019 11:10:12 AM
 * Author : Ruben
 */
-
+#define F_CPU 16000000UL
 #include <avr/io.h>
-
+#include <util/delay.h>
+#include <stdio.h>
 
 int main(void)
 {
-	/* Replace with your application code */
-	while (1)
+	
+	DDRB &= ~(1<<DDB7);
+	while(1)
 	{
+		if(!(PINB & (1<<PINB7))){
+			PORTB = 0B00111111;
+			PORTD = 0B11111100;
+			_delay_ms(1000);
+			PORTB = 0B00000000;
+			PORTD = 0B00000000;
+			_delay_ms(1000);
+		}
+		
 	}
+	
 }
 
