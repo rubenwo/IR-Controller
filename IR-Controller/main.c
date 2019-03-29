1da2d603b0ec450539aeab60718ab57db46aeac9
BartAvans <b.vanes1@student.avans.nl>
2019-03-29 17:42:47 +0100

    [Added] IR ding

--- IR-Controller/main.c
+++ IR-Controller/main.c
@@ -6,25 +6,52 @@
 */
 #define F_CPU 16000000UL
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
-#include <stdio.h>
-
-int main(void)
+#include <stdint.h>
+#include <stdlib.h>
+int main()
 {
 	
-	DDRB &= ~(1<<DDB7);
-	while(1)
-	{
-		if(!(PINB & (1<<PINB7))){
-			PORTB = 0B00111111;
-			PORTD = 0B11111100;
-			_delay_ms(1000);
-			PORTB = 0B00000000;
-			PORTD = 0B00000000;
-			_delay_ms(1000);
+    /* Set PB5 to output */
+	
+	//while(1){
+	//
+	//char string[16];
+	//int temp = ADCH;
+	//sprintf(string, "Temp: 0x%.2X", temp);
+	//PORTC = ADCH;
+	//_delay_ms(1000);
+	//DDRC = 0b00000000;
+	//if (PC3 >= 0)
+	//{
+		//DDRD = 0b11111111;
+		//PORTD |= (1<< PD5);
+		//_delay_ms(1000);
+		//PORTD &= (1<<PD5);
+	//}
+	//}
+    //return 0;
+	
+	DDRC = 0x00;
+	DDRD = 0xFF;
+	ADCSRA = 0b10000000;
+	ADMUX = 0b01100001;
+	int now = 2;
+	while(1){
+		ADCSRA |= (1<<ADSC);
+		while((ADCSRA & (1<< ADIF)) == 0){
+		int temp = ADC;
+		if(temp != now){
+			PORTD ^= (1<<PD5);
+			int i;
+			for(i =0; i<1000; i++)
+				_delay_ms(1);
+			PORTD ^= (1<<PD5);
+			now = temp;
+		}
 		}
 		
 	}
-	
 }
-
+	
